8b646729e30e270c83b8aefc6fd87c722ab6b700
Dhindsa91 <arvind.dhindsa1@gmail.com>
2018-03-20 18:38:15 -0700

    fixed userroles issues

--- src/main/java/com/devopsbuddy/backend/persistence/domain/backend/UserRole.java
+++ src/main/java/com/devopsbuddy/backend/persistence/domain/backend/UserRole.java
@@ -3,9 +3,7 @@ package com.devopsbuddy.backend.persistence.domain.backend;
 import javax.persistence.*;
 import java.io.Serializable;
 
-/**
- * Created by tedonema on 28/03/2016.
- */
+
 @Entity
 @Table(name = "user_role")
 public class UserRole implements Serializable {
